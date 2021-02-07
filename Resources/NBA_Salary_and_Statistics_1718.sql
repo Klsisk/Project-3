d3ba08ed01b4249f8de6c49eb08c521aa0ff6dc9
Kendall Sisk <69765842+Klsisk@users.noreply.github.com>
2021-02-07 14:41:44 -0500

    updated files

--- Resources/NBA_Salary_and_Statistics_1718.sql
+++ Resources/NBA_Salary_and_Statistics_1718.sql
@@ -6,6 +6,23 @@ CREATE TABLE salary (
 	salary INT
 );
 
+CREATE TABLE allstar (
+	id SERIAL,
+	player VARCHAR(100),
+	position VARCHAR(100),
+	team VARCHAR(100)	
+);
+
+CREATE TABLE team_stats (
+	id SERIAL,
+	Team VARCHAR(100),
+	win INT,
+	loss INT,
+	PSG DEC,
+	PAG DEC,
+	SRS DEC
+);
+
 CREATE TABLE stats (
 	id SERIAL,
 	player VARCHAR(100),
@@ -17,25 +34,34 @@ CREATE TABLE stats (
 	points_per_game FLOAT	
 );
 
-CREATE TABLE allstar (
-	id SERIAL,
-	player VARCHAR(100),
-	position VARCHAR(100),
-	team VARCHAR(100)	
-);
+select * from stats;
+select * from salary;
+select * from team_stats;
+select * from allstar;
 
-CREATE TABLE team_stats (
-	id SERIAL,
-	Team VARCHAR(100),
-	W INT,
-	L INT,
-	PSG FLOAT,
-	PAG FLOAT,
-	SRS FLOAT
-);
+SELECT player, team, sum(salary)
+FROM public.salary
+group by player, team;
 
--- Query to check successful load
-select * from salary
-select * from stats
-select * from allstar
-select * from team_stats
+SELECT team, sum(salary) as "Total Salary Paid"
+FROM public.salary
+group by team;
+
+SELECT salary.team, team_stats.win, team_stats.loss, sum(salary.salary)
+FROM public.salary, team_stats 
+where salary.team = team_stats.team
+group by salary.team, team_stats.win, team_stats.loss
+order by salary.team;
+
+select stats.player, salary.salary, stats.points_per_game, ROUND(stats.total_games * stats.points_per_game)  
+as "totalpoints", ROUND(salary.salary/stats.points_per_game) as "bangforbuck"
+from stats, salary where stats.player = salary.player and stats.points_per_game > 0
+group by stats.player, salary.salary, stats.points_per_game, totalpoints, bangforbuck
+order by bangforbuck DESC;
+
+
+
+
+
+   
+
